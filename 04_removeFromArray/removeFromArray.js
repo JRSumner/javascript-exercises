43c54af22d243439f621af3abb37d9acfcd64028
JRSumner <joeraysumner@gmail.com>
2023-02-10 22:39:50 +0000

    Start 04_removeFromArray, passes 5/7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num1, num2, num3, num4) {
+  arr.forEach((element) => {
+    if (
+      element === num1 ||
+      element === num2 ||
+      element === num3 ||
+      element === num4
+    ) {
+      arr.splice(arr.indexOf(element), 1);
+    }
+  });
 
+  return arr;
 };
 
 // Do not edit below this line
