4ae4fbac9110dc0b9f49443486d8d476f51caec7
JRSumner <joeraysumner@gmail.com>
2023-02-10 22:26:38 +0000

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let result = [];
+  const stringArr = str.split("");
+  const inputStrLength = stringArr.length;
 
+  for (let i = 0; i < inputStrLength; i++) {
+    result += stringArr.pop();
+  }
+
+  return result.toString();
 };
 
 // Do not edit below this line
