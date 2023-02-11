96401d025ed46338d727d0467f1480e70a1a901d
JRSumner <joeraysumner@gmail.com>
2023-02-11 08:13:48 +0000

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,5 @@
-const removeFromArray = function (arr, num1, num2, num3, num4) {
-  for (let i = 0; i < arr.length; i++) {
-    if (
-      arr[i] === num1 ||
-      arr[i] === num2 ||
-      arr[i] === num3 ||
-      arr[i] === num4
-    ) {
-      arr.splice(arr.indexOf(arr[i]), 1);
-      i = 0;
-    }
-  }
-
-  return arr;
+const removeFromArray = function (array, ...valuesToRemove) {
+  return array.filter((item) => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
